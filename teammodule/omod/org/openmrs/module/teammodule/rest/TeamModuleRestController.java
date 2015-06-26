4d03d07828c4279944bbd0cd8d611e934fcdff73
Muhammad Safwan <muhammad.safwan@irdinformatics.org>
2015-06-26 14:12:32 +0500

    Adding the teammodule folder

---
+++ teammodule/omod/org/openmrs/module/teammodule/rest/TeamModuleRestController.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package org.openmrs.module.teammodule.rest;
+
+//import org.openmrs.module.webservices.rest.web.RestConstants;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author Muhammad Safwan
+ *
+ */
+
+@Controller
+@RequestMapping("/rest/" +/*+ RestConstants.VERSION_1 + */"/teammodule")
+public class TeamModuleRestController  {
+
+}
